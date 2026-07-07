09178326260267dd42ae0c5c3363c1d75e388c42
prashant9429 <165670969+prashant9429@users.noreply.github.com>
2026-07-07 19:08:23 +0530

    Add responsive navbar component

---
+++ includes/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+<a class="navbar-brand" href="index.php">TechNova Solutions</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav ms-auto">
+<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+<li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
+<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+</ul>
+</div>
+</div>
+</nav>
